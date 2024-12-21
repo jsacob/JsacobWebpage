c0197fd0eed71551c599645555720360415e08e5
jacob <jacobmofficial@outlook.com>
2024-12-21 20:02:47 +0000

    header

--- Frontend/src/components/header.tsx
+++ Frontend/src/components/header.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
+import { FaGithub, FaRegMoon } from "react-icons/fa";
+import { useState, useEffect } from "react";
+const navItems = ["About", "Portfolio", "Projects", "Blog"];
 
 export default function Header() {
+  const [mobileView, setmobileView] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth > 767) {
+      setmobileView(false);
+    } else if (window.innerWidth < 767) {
+      setmobileView(true);
+    }
+  }, []);
+
   return (
-    <div className="container max-w-full p-4 bg-red-950 inline-flex space-x-8">
+    <header className="pb-7 container max-w-full p-4 inline-flex space-x-8 justify-center flex-grow">
       {/* Left */}
-      <div className="bg-black font-bold text-xl">
+      <div className="font-bold text-2xl min-w-56 max-w-48 flex justify-start pt-3 lex-shrink-0">
         <Typewriter
           options={{
             autoStart: true,
@@ -29,15 +41,22 @@ export default function Header() {
       </div>
 
       {/* Center (Navigation options) */}
-      <div className="flex space-x-14 font-bold text-white text-xl bg-blue-400 flex-grow justify-center">
-        <div>About</div>
-        <div>Portfolio</div>
-        <div>Projects</div>
-        <div>Blog</div>
-      </div>
+      <nav className="flex space-x-12 text-white text-2xl flex-grow justify-center pr-24 pt-3">
+        {navItems.map((item) => (
+          <div
+            className="hover:text-blue-500 hover:translate-y-3 transition-transform duration-500 ease-in-out cursor-pointer"
+            key={item}
+          >
+            {item}
+          </div>
+        ))}
+      </nav>
 
-      {/* Right (Empty) */}
-      <div className="flex-shrink-0">hello</div>
-    </div>
+      {/* Right (Empty or dynamic content) */}
+      <div className="flex-shrink-0 pt-3 text-3xl inline-flex space-x-6">
+        <FaGithub className="text-white hover:text-purple-500 hover:translate-y-3 transition-transform-colors duration-300 ease-in-out cursor-pointer" />
+        <FaRegMoon className="text-white hover:text-yellow-300 hover:translate-y-3 transition-transform-colors duration-300 ease-in-out cursor-pointer" />
+      </div>
+    </header>
   );
 }

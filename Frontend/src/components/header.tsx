e6e7a2dab3258d1491a0c88aef6154f789aefe54
jacob <jacobmofficial@outlook.com>
2024-12-22 18:48:04 +0000

    header

--- Frontend/src/components/header.tsx
+++ Frontend/src/components/header.tsx
@@ -15,17 +15,17 @@ const navItems = [
 export default function Header() {
   useEffect(() => {
     AOS.init();
-  });
+  }, []);
   return (
     <>
       <div data-aos="fade-down" data-aos-duration="1500">
         <header className="container max-w-full p-4 inline-flex space-x-8 justify-center flex-grow">
           {/*Center Content*/}
-          <nav className="flex space-x-12 text-white text-xl flex-grow justify-center">
+          <nav className="flex space-x-12 text-white md:text-lg flex-grow justify-center">
             {navItems.map((item) => (
               <a
                 href={item.link}
-                className="hover:text-blue-500 hover:translate-y-2 transition-transform duration-300 ease-in-out"
+                className="hover:text-blue-600 hover:translate-y-2 transition-thansform duration-300 ease-in-out"
                 key={item.name}
               >
                 {item.name}
